68adeff34b630cd62dba7838669a6c2cff63df1f
Andrew Stannard <andrew.stannard@gmail.com>
2021-04-14 20:03:07 +0100

    Initial Commit

---
+++ Shared/TahDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahDoodleApp.swift
+//  Shared
+//
+//  Created by Andrew Stannard on 14/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TahDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
