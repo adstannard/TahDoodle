2de3fcda5a1258c10778fa6e55bff662664ce268
Andrew Stannard <andrew.stannard@gmail.com>
2021-04-15 10:02:54 +0100

    Build first stage and update. No persistence yet.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let taskStore: TaskStore
+    @State private var newTaskTitle = ""
+    
+    private var newTaskView: some View {
+        HStack {
+            TextField("Something to do", text: $newTaskTitle)
+            Button("Add Task") {
+                let task = Task(title: newTaskTitle)
+                taskStore.add(task)
+                newTaskTitle = ""
+            }.disabled(newTaskTitle.isEmpty)
+        }.padding()
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            newTaskView
+            TaskListView(taskStore: taskStore)
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(taskStore: .sample)
     }
 }
---
+++ Shared/TaskListView.swift
@@ -0,0 +1,32 @@
+//
+//  TaskListView.swift
+//  TahDoodle
+//
+//  Created by Andrew Stannard on 14/04/2021.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    
+    @ObservedObject var taskStore: TaskStore
+    
+    var body: some View {
+        List {
+            ForEach(taskStore.tasks) { task in
+                TaskView(title: task.title)
+            }.onDelete { indexSet in
+                indexSet.forEach { index in
+                    let task = taskStore.tasks[index]
+                    taskStore.remove(task)
+                }
+            }
+        }
+    }
+}
+
+struct TaskListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskListView(taskStore: .sample)
+    }
+}
